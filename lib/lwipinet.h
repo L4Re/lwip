f625dfc0f3b7fe3eef3795c53bd0dfa3be8bcc30
Marcus Haehnel <marcus.haehnel@kernkonzept.com>
2025-05-13 15:42:09 +0200

    Be more POSIX compliant and don't require BSD/GNU Source
    Use memcpy to convert ip6_addr instead of GNU specific struct members.
    
    Also require _BSD_SOURCE support for the sockets.c file, since this
    needs the GNU/BSD extension `ifreq` structure to implement the
    SO_BINDTODEVICE socket option.
    
    Change-Id: I9e002f1f7aab41ee425e12997df0f77dacb18a35

--- lib/lwipinet.h
+++ lib/lwipinet.h
@@ -15,15 +15,10 @@
 #endif /* LWIP_IPV4 */
 
 #if LWIP_IPV6
-#define inet6_addr_from_ip6addr(target_in6addr, source_ip6addr) {(target_in6addr)->s6_addr32[0] = (source_ip6addr)->addr[0]; \
-                                                                 (target_in6addr)->s6_addr32[1] = (source_ip6addr)->addr[1]; \
-                                                                 (target_in6addr)->s6_addr32[2] = (source_ip6addr)->addr[2]; \
-                                                                 (target_in6addr)->s6_addr32[3] = (source_ip6addr)->addr[3];}
-#define inet6_addr_to_ip6addr(target_ip6addr, source_in6addr)   {(target_ip6addr)->addr[0] = (source_in6addr)->s6_addr32[0]; \
-                                                                 (target_ip6addr)->addr[1] = (source_in6addr)->s6_addr32[1]; \
-                                                                 (target_ip6addr)->addr[2] = (source_in6addr)->s6_addr32[2]; \
-                                                                 (target_ip6addr)->addr[3] = (source_in6addr)->s6_addr32[3]; \
-                                                                 ip6_addr_clear_zone(target_ip6addr);}
+#define inet6_addr_from_ip6addr(target_in6addr, source_ip6addr) \
+  { __builtin_memcpy((target_in6addr)->s6_addr, (source_ip6addr)->addr, sizeof((target_in6addr)->s6_addr)); }
+#define inet6_addr_to_ip6addr(target_ip6addr, source_in6addr) \
+  { __builtin_memcpy((target_ip6addr)->addr, (source_in6addr)->s6_addr32, sizeof((target_ip6addr)->addr)); ip6_addr_clear_zone(target_ip6addr); }
 #endif /* LWIP_IPV6 */
 
 #endif /* LWIP_HDR_EXTERNAL_INET_H */
